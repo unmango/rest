0f06ac42fbc758672a8b8ecc58d30268105a0084
Erik Rasmussen <ecr.referee@hotmail.com>
2020-04-01 21:46:18 -0500

    Trying out the interface

---
+++ src/UnMango.Rest/IRestClient.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnMango.Rest
+{
+    public interface IRestClient
+    {
+        Task<T> DeleteAsync<T>(string route, CancellationToken cancellationToken = default);
+
+        Task<T> GetAsync<T>(string route, CancellationToken cancellationToken = default);
+
+        Task<TResult> PatchAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+
+        Task<TResult> PostAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+
+        Task<TResult> PutAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+    }
+}
