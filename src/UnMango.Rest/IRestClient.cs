3ecb162bf54f29a8a700b607be68d1d1fd35017d
Erik Rasmussen <ecr.referee@hotmail.com>
2020-04-10 21:26:00 -0500

    Use uri with string extension methods

--- src/UnMango.Rest/IRestClient.cs
+++ src/UnMango.Rest/IRestClient.cs
@@ -1,18 +1,22 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace UnMango.Rest
 {
+    /// <summary>
+    /// A typed rest client abstraction.
+    /// </summary>
     public interface IRestClient
     {
-        Task<T> DeleteAsync<T>(string route, CancellationToken cancellationToken = default);
+        Task<T> DeleteAsync<T>(Uri requestUri, CancellationToken cancellationToken = default);
 
-        Task<T> GetAsync<T>(string route, CancellationToken cancellationToken = default);
+        Task<T> GetAsync<T>(Uri requestUri, CancellationToken cancellationToken = default);
 
-        Task<TResult> PatchAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+        Task<TResult> PatchAsync<TRequest, TResult>(Uri requestUri, TRequest request, CancellationToken cancellationToken = default);
 
-        Task<TResult> PostAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+        Task<TResult> PostAsync<TRequest, TResult>(Uri requestUri, TRequest request, CancellationToken cancellationToken = default);
 
-        Task<TResult> PutAsync<TRequest, TResult>(string route, TRequest request, CancellationToken cancellationToken = default);
+        Task<TResult> PutAsync<TRequest, TResult>(Uri requestUri, TRequest request, CancellationToken cancellationToken = default);
     }
 }
---
+++ src/UnMango.Rest/RestClientExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnMango.Rest
+{
+    /// <summary>
+    /// Extensions for <see cref="IRestClient"/>
+    /// </summary>
+    public static class RestClientExtensions
+    {
+        public static Task<T> DeleteAsync<T>(
+            this IRestClient client,
+            string requestUri,
+            CancellationToken cancellationToken = default)
+        {
+            return client.DeleteAsync<T>(new Uri(requestUri), cancellationToken);
+        }
+
+        public static Task<T> GetAsync<T>(
+            this IRestClient client,
+            string requestUri,
+            CancellationToken cancellationToken = default)
+        {
+            return client.GetAsync<T>(new Uri(requestUri), cancellationToken);
+        }
+
+        public static Task<TResult> PatchAsync<TRequest, TResult>(
+            this IRestClient client,
+            string requestUri,
+            TRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            return client.PatchAsync<TRequest, TResult>(new Uri(requestUri), request, cancellationToken);
+        }
+
+        public static Task<TResult> PostAsync<TRequest, TResult>(
+            this IRestClient client,
+            string requestUri,
+            TRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            return client.PostAsync<TRequest, TResult>(new Uri(requestUri), request, cancellationToken);
+        }
+
+        public static Task<TResult> PutAsync<TRequest, TResult>(
+            this IRestClient client,
+            string requestUri,
+            TRequest request,
+            CancellationToken cancellationToken = default)
+        {
+            return client.PutAsync<TRequest, TResult>(new Uri(requestUri), request, cancellationToken);
+        }
+    }
+}
