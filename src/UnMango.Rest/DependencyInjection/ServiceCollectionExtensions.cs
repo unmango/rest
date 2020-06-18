1f6569017ab1545b3a60a59c27da1d2bde0fb26e
Erik Rasmussen <ecr.referee@hotmail.com>
2020-06-18 17:18:33 -0500

    WIP with client factory

---
+++ src/UnMango.Rest/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+    }
+}
---
+++ src/UnMango.Rest/IRestClientFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace UnMango.Rest
+{
+    public interface IRestClientFactory
+    {
+        /// <summary>
+        /// Creates a <see cref="IRestClient"/> with configuration corresponding to <paramref name="name"/>.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="IRestClient"/> to create.</param>
+        /// <returns>The configured <see cref="IRestClient"/>.</returns>
+        IRestClient Create(string name);
+    }
+}
