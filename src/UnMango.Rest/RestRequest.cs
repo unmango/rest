0510cfaec856773828d09a5d871d777f8cd8b24c
Erik Rasmussen <ecr.referee@hotmail.com>
2020-07-11 18:40:33 -0500

    Update RestRequest impl

--- src/UnMango.Rest/RestRequest.cs
+++ src/UnMango.Rest/RestRequest.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Net.Http;
 
 namespace UnMango.Rest
 {
+    /// <summary>
+    /// A REST request.
+    /// </summary>
     public sealed class RestRequest : IRestRequest
     {
-        private readonly Dictionary<string, string> _queryParameters;
-        
         public RestRequest(IRestClient client)
         {
-            _queryParameters = new Dictionary<string, string>();
             Client = client ?? throw new ArgumentNullException(nameof(client));
+            QueryParameters = new Dictionary<string, string>();
+        }
+
+        public RestRequest(IRestClient client, HttpMethod method)
+        {
+            Client = client ?? throw new ArgumentNullException(nameof(client));
+            Method = method;
+            QueryParameters = new Dictionary<string, string>();
         }
         
+        /// <inheritdoc />
         public IRestClient Client { get; }
 
-        HttpMethod? Method { get; }
+        /// <inheritdoc />
+        public HttpMethod? Method { get; } = null;
 
-        public IEnumerable<KeyValuePair<string, string>> QueryParameters => _queryParameters;
+        /// <inheritdoc />
+        public IReadOnlyDictionary<string, string> QueryParameters { get; }
     }
 }
