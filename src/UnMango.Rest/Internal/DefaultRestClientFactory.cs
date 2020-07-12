832e8bc92307cba8610a3e22f4e8367e7cd1396d
Erik Rasmussen <ecr.referee@hotmail.com>
2020-07-11 21:51:24 -0500

    Remove custom httpclientfactory
    and update comments to reflect changes

--- src/UnMango.Rest/Internal/DefaultRestClientFactory.cs
+++ src/UnMango.Rest/Internal/DefaultRestClientFactory.cs
@@ -1,36 +1,28 @@
-﻿using Microsoft.Extensions.Options;
+﻿using System.Net.Http;
+using Microsoft.Extensions.Options;
 
 namespace UnMango.Rest.Internal
 {
     /// <inheritdoc />
     public class DefaultRestClientFactory : IRestClientFactory
     {
-        private readonly IHttpClientFactory _clientFactory;
         private readonly IOptionsMonitor<RestClientOptions> _optionsMonitor;
 
         /// <summary>
-        /// Initializes a new instance of a <see cref="DefaultHttpClientFactory"/> to
-        /// create <see cref="IRestClient"/>s using <paramref name="clientFactory"/>
-        /// to create the underlying <see cref="System.Net.Http.HttpClient"/> and <paramref name="optionsMonitor"/>
-        /// to fetch options to configure the <see cref="IRestClient"/>.
+        /// Initializes a new instance of a <see cref="DefaultRestClientFactory"/> that instantiates a new
+        /// <see cref="System.Net.Http.HttpClient"/> to use for each request to <see cref="Create"/>
+        /// and <paramref name="optionsMonitor"/> to fetch options to configure the <see cref="IRestClient"/>.
         /// </summary>
-        /// <param name="clientFactory">
-        /// Used to create the underlying <see cref="System.Net.Http.HttpClient"/>
-        /// used by the <see cref="IRestClient"/>.
-        /// </param>
         /// <param name="optionsMonitor">Fetches options to configure the created <see cref="IRestClient"/>.</param>
-        public DefaultRestClientFactory(
-            IHttpClientFactory clientFactory,
-            IOptionsMonitor<RestClientOptions> optionsMonitor)
+        public DefaultRestClientFactory(IOptionsMonitor<RestClientOptions> optionsMonitor)
         {
-            _clientFactory = clientFactory;
             _optionsMonitor = optionsMonitor;
         }
 
         /// <inheritdoc />
         public IRestClient Create(string name)
         {
-            var client = _clientFactory.CreateClient(name);
+            var client = new HttpClient();
             var options = _optionsMonitor.Get(name);
 
             return new RestClient(client, options.ClientSerializers[name]);
