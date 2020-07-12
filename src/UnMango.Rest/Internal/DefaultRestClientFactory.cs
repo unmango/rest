d204df3a069c79b10b0058ee79b42490fa063838
Erik Rasmussen <ecr.referee@hotmail.com>
2020-07-11 21:19:57 -0500

    Add comments to DefaultRestClientFactory

--- src/UnMango.Rest/Internal/DefaultRestClientFactory.cs
+++ src/UnMango.Rest/Internal/DefaultRestClientFactory.cs
@@ -1,13 +1,24 @@
-﻿using System.Net.Http;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.Extensions.Options;
 
 namespace UnMango.Rest.Internal
 {
+    /// <inheritdoc />
     public class DefaultRestClientFactory : IRestClientFactory
     {
         private readonly IHttpClientFactory _clientFactory;
         private readonly IOptionsMonitor<RestClientOptions> _optionsMonitor;
 
+        /// <summary>
+        /// Initializes a new instance of a <see cref="DefaultHttpClientFactory"/> to
+        /// create <see cref="IRestClient"/>s using <paramref name="clientFactory"/>
+        /// to create the underlying <see cref="System.Net.Http.HttpClient"/> and <paramref name="optionsMonitor"/>
+        /// to fetch options to configure the <see cref="IRestClient"/>.
+        /// </summary>
+        /// <param name="clientFactory">
+        /// Used to create the underlying <see cref="System.Net.Http.HttpClient"/>
+        /// used by the <see cref="IRestClient"/>.
+        /// </param>
+        /// <param name="optionsMonitor">Fetches options to configure the created <see cref="IRestClient"/>.</param>
         public DefaultRestClientFactory(
             IHttpClientFactory clientFactory,
             IOptionsMonitor<RestClientOptions> optionsMonitor)
@@ -16,6 +27,7 @@ namespace UnMango.Rest.Internal
             _optionsMonitor = optionsMonitor;
         }
 
+        /// <inheritdoc />
         public IRestClient Create(string name)
         {
             var client = _clientFactory.CreateClient(name);
