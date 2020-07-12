f65b1604c1aaec00ff7d13493edda88251319c57
Erik Rasmussen <ecr.referee@hotmail.com>
2020-07-12 11:43:46 -0500

    Add public static RestClient for non-DI scenarios

---
+++ src/UnMango.Rest/RestClient.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Net.Http;
+using UnMango.Rest.Serialization;
+
+namespace UnMango.Rest
+{
+    using InternalRestClient = Internal.RestClient;
+    
+    public static class RestClient
+    {
+        public static IRestClient Create(HttpClient client, ISerializerCollection? serializers)
+        {
+            if (client == null) throw new ArgumentNullException(nameof(client));
+            serializers ??= SerializerCollection.Instance;
+            
+            return new InternalRestClient(client, serializers);
+        }
+    }
+}
